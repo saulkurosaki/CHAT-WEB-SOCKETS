86013896fc1309a9ce7952be207dd83edc6a6822
saulkurosaki <saulkurosaki13@gmail.com>
2024-09-05 11:11:52 -0600

    install the react-router-dom library dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
-  );
+  return <main className="w-full h-[100dvh] bg-green-600"></main>;
 }
 
 export default App;
