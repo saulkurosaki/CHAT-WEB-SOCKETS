644c03fb206a9df82e8fcf07bbb6289681971ed5
saulkurosaki <saulkurosaki13@gmail.com>
2024-09-05 14:51:58 -0600

    implement the complete V0.dev project basic layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/pages/auth/Login";
 import Register from "./components/pages/auth/Register";
 import ChatRooms from "./components/pages/ChatRooms";
+import ChatApp from "./components/ChatApp";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<ChatRooms />} />
-      </Routes>
-    </Router>
-  );
+  return <ChatApp />;
 }
 
 export default App;
