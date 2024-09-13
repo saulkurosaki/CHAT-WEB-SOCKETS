f0ac4803fc739af7c2032a3d72f117b301ad9b46
saulkurosaki <saulkurosaki13@gmail.com>
2024-09-13 14:18:47 -0700

    feat(Login): Optimize the Login component

--- frontend/src/components/pages/auth/Login.tsx
+++ frontend/src/components/pages/auth/Login.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 import { useUserStore } from "@/store";
 
@@ -28,7 +28,6 @@ export const Login = () => {
     }),
     onSubmit: (values) => {
       setIsLoading(true);
-
       setTimeout(() => {
         const user = {
           name: "Cristiano",
@@ -36,24 +35,23 @@ export const Login = () => {
           username: "cr7",
           email: values.email,
           phoneNumber: "318559036",
-          profileImage: 'https://pbs.twimg.com/profile_images/1594446880498401282/o4L2z8Ay_400x400.jpg',
-        }
-
-        localStorage.setItem('token', '123456');
-        localStorage.setItem('user', JSON.stringify(user));
+          profileImage:
+            "https://pbs.twimg.com/profile_images/1594446880498401282/o4L2z8Ay_400x400.jpg",
+        };
+        localStorage.setItem("token", "123456");
+        localStorage.setItem("user", JSON.stringify(user));
         setUser(user);
         navigate("/", { replace: true });
         setIsLoading(false);
       }, 2000);
     },
-  })
+  });
 
   return (
     <form onSubmit={formik.handleSubmit}>
       <h1 className="text-2xl font-bold mb-4 text-center text-teal-600">
         Login
       </h1>
-
       <Input
         type="email"
         name="email"
@@ -64,7 +62,6 @@ export const Login = () => {
         onChange={formik.handleChange}
         onBlur={formik.handleBlur}
       />
-
       <Input
         type="password"
         name="password"
@@ -75,16 +72,14 @@ export const Login = () => {
         onChange={formik.handleChange}
         onBlur={formik.handleBlur}
       />
-
       <Button type="submit" className="w-full mb-2">
         {isLoading ? <Spinner size="size-6" /> : "Login"}
       </Button>
-
       <Button
         type="button"
         variant="link"
         className="w-full"
-        onClick={() => navigate('/auth/register')}
+        onClick={() => navigate("/auth/register")}
       >
         Don't have an account? Register
       </Button>
