dfdcd43703d378af09bc8326e729bb7880e79025
saulkurosaki <saulkurosaki13@gmail.com>
2024-09-29 14:25:55 -0600

    feat(ContactCard): implement the complete ContactCard component

--- frontend/src/components/shared/ContactCard.tsx
+++ frontend/src/components/shared/ContactCard.tsx
@@ -1,5 +1,47 @@
-const ContactCard = () => {
-  return <div>ContactCard</div>;
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { Button } from "../ui/button";
+
+interface ContactCardProps {
+  contact: {
+    _id: string;
+    name: string;
+  };
+  onDelete: (contactId: string) => void;
+  onClick: () => void;
+}
+
+const ContactCard = ({ contact, onDelete, onClick }: ContactCardProps) => {
+  const getInitials = (name: string) => {
+    return name
+      .split(" ")
+      .map((n) => n[0])
+      .join("")
+      .toUpperCase();
+  };
+
+  return (
+    <div
+      key={contact._id}
+      className="flex items-center p-2 cursor-pointer hover:bg-gray-100 justify-between"
+      onClick={onClick}
+    >
+      <div className="flex items-center">
+        <Avatar className="h-10 w-10 mr-3">
+          <AvatarFallback>{getInitials(contact.name)}</AvatarFallback>
+        </Avatar>
+        <span>{contact.name}</span>
+      </div>
+      <Button
+        onClick={(e) => {
+          e.stopPropagation(); // Evita que se active el evento de clic en el contacto
+          onDelete(contact._id);
+        }}
+        className="w-7 h-auto ml-2 text-gray-500 bg-transparent rounded-full p-1 hover:text-red-500"
+      >
+        X
+      </Button>
+    </div>
+  );
 };
 
 export default ContactCard;
