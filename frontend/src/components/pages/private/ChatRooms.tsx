3fe0af76ccc36a40a33ecd204f37527ec22ea6d9
saulkurosaki <saulkurosaki13@gmail.com>
2024-10-03 13:02:14 -0600

    feat(ChatRooms): install and implement the ldsr waveform loader

--- frontend/src/components/pages/private/ChatRooms.tsx
+++ frontend/src/components/pages/private/ChatRooms.tsx
@@ -14,15 +14,18 @@ import { IChat } from "@/interfaces";
 import Footer from "@/components/shared/Footer";
 import { findUserById, getUserChatRooms } from "@/services/private";
 
+import "ldrs/waveform";
+
 export const ChatRooms = () => {
   const { search } = useSearchStore();
   const [chats, setChats] = useState<IChat[]>([]);
   const { user } = useUserStore();
-
+  const [loading, setLoading] = useState(true); // Estado para el loader
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchChatRooms = async () => {
+      setLoading(true);
       if (user?._id) {
         const response = await getUserChatRooms(user._id);
         if (response.ok) {
@@ -32,6 +35,7 @@ export const ChatRooms = () => {
           }
         } else {
           console.error(response.error);
+          setLoading(false);
         }
       }
     };
@@ -41,6 +45,7 @@ export const ChatRooms = () => {
 
   useEffect(() => {
     const updatePrivateRoomsAvatarAndName = async () => {
+      setLoading(true);
       const updatedChats = await Promise.all(
         chats.map(async (chat) => {
           if (chat.chatRoomType === "private") {
@@ -68,6 +73,7 @@ export const ChatRooms = () => {
       );
 
       setChats(updatedChats);
+      setLoading(false); // Ocultar el loader una vez que se complete la actualización
     };
 
     if (chats.length > 0) {
@@ -81,49 +87,60 @@ export const ChatRooms = () => {
 
   return (
     <>
-      <ScrollArea
-        className="w-full flex-grow bg-gray-100"
-        style={{ height: "calc(100vh - 100px)" }}
-      >
-        {filteredChats.length > 0 ? (
-          filteredChats.map((chat) => (
-            <div
-              key={chat._id}
-              className="flex items-center p-4 border-b cursor-pointer hover:bg-gray-100"
-              onClick={() => navigate(`/chat/${chat._id}`)}
-            >
-              <Avatar className="h-12 w-12 mr-4">
-                {chat.avatar ? (
-                  <AvatarImage src={chat.avatar} alt={chat.name} />
-                ) : (
-                  <AvatarFallback>
-                    {handleGetInitials(chat.name)}
-                  </AvatarFallback>
-                )}
-              </Avatar>
-              <div className="flex-grow">
-                <h2 className="font-semibold">{chat.name}</h2>
-                <p className="text-sm text-gray-500">
-                  {chat.chatRoomType === "public"
-                    ? "Group Chat"
-                    : "Personal Chat"}
-                </p>
+      {loading ? ( // Mostrar loader si está cargando
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 z-50">
+          <l-waveform size={128} speed={1} stroke={8} color="#40EB97" />
+        </div>
+      ) : (
+        <>
+          <ScrollArea
+            className="w-full flex-grow bg-gray-100"
+            style={{ height: "calc(100vh - 100px)" }}
+          >
+            {filteredChats.length > 0 ? (
+              filteredChats.map((chat) => (
+                <div
+                  key={chat._id}
+                  className="flex items-center p-4 border-b cursor-pointer hover:bg-gray-100"
+                  onClick={() => navigate(`/chat/${chat._id}`)}
+                >
+                  <Avatar className="h-12 w-12 mr-4">
+                    {chat.avatar ? (
+                      <AvatarImage src={chat.avatar} alt={chat.name} />
+                    ) : (
+                      <AvatarFallback>
+                        {handleGetInitials(chat.name)}
+                      </AvatarFallback>
+                    )}
+                  </Avatar>
+                  <div className="flex-grow">
+                    <h2 className="font-semibold">{chat.name}</h2>
+                    <p className="text-sm text-gray-500">
+                      {chat.chatRoomType === "public"
+                        ? "Group Chat"
+                        : "Personal Chat"}
+                    </p>
+                  </div>
+                  {chat.unreadMessages > 0 && (
+                    <Badge
+                      variant="secondary"
+                      className="bg-green-500 text-white"
+                    >
+                      {chat.unreadMessages}
+                    </Badge>
+                  )}
+                </div>
+              ))
+            ) : (
+              <div className="flex items-center justify-center h-full text-gray-500">
+                No chats exist at the moment. Create a new one to start
+                chatting.
               </div>
-              {chat.unreadMessages > 0 && (
-                <Badge variant="secondary" className="bg-green-500 text-white">
-                  {chat.unreadMessages}
-                </Badge>
-              )}
-            </div>
-          ))
-        ) : (
-          <div className="flex items-center justify-center h-full text-gray-500">
-            No chats exist at the moment. Create a new one to start chatting.
-          </div>
-        )}
-      </ScrollArea>
-
-      <Footer />
+            )}
+          </ScrollArea>
+          <Footer />
+        </>
+      )}
     </>
   );
 };
